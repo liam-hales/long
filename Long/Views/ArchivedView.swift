e1e714a7d74bbb7471093cac9d2676f31d47a331
Liam Hales <liam.hales96@gmail.com>
2026-09-17 22:28:39 +0100

    Added `TasksView` and `ArchivedView` views

---
+++ Long/Views/ArchivedView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+import SwiftData
+
+/// Used to display the users
+/// archived tasks
+struct ArchivedView: View {
+    
+  /// Query for the archived tasks stored on device
+  /// ordered by ones that were completed first
+  @Query(
+    filter: #Predicate<TaskItem> {
+      $0.isArchived == true
+    },
+    sort: \TaskItem.completedDate,
+    order: .reverse
+  )
+  private var tasks: [TaskItem];
+  
+  var body: some View {
+    NavigationStack {
+      List(tasks) { task in
+        Text(task.title);
+      }
+      .navigationTitle("Archived");
+    }
+  }
+}
---
+++ Long/Views/TasksView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+import SwiftData
+
+/// Used to display the users
+// current outstanding tasks
+struct TasksView: View {
+
+  @Environment(AppState.self)
+  private var _appState: AppState;
+
+  /// Query for the  unarchived tasks stored on
+  /// device ordered by ones that are due first
+  @Query(
+    filter: #Predicate<TaskItem> {
+      $0.isArchived == false
+    },
+    sort: \TaskItem.dueDate,
+    order: .reverse
+  )
+  private var tasks: [TaskItem];
+
+  var body: some View {
+
+    @Bindable
+    var appState = _appState;
+
+    NavigationStack {
+      List(tasks) { task in
+        Text(task.title);
+      }
+      .toolbar {
+        ToolbarView(taskFilter: $appState.taskFilter)
+      };
+    }
+  }
+}
