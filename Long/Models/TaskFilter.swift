5ddecef32669e7b51a38533863ab556695451643
Liam Hales <liam.hales96@gmail.com>
2026-09-17 22:22:40 +0100

    Added `TaskFilter` enum used to describe all ways the user can filter their tasks

---
+++ Long/Models/TaskFilter.swift
@@ -0,0 +1,29 @@
+import Foundation
+import LucideSwift
+
+/// Describes all ways the user
+/// can filter their tasks
+enum TaskFilter: String, CaseIterable, Identifiable {
+  case date;
+  case category;
+    
+  var id: Self {
+    self;
+  }
+  
+  /// Describes the task filter title
+  var title: String {
+    switch self {
+      case .date: "Date";
+      case .category: "Category";
+    }
+  }
+  
+  /// Describes the task filter icon
+  var icon: LucideIconName {
+    switch self {
+      case .date: .calendarDays;
+      case .category: .tags;
+    }
+  }
+}
