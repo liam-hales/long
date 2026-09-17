ef46d6b001704cbf7934d8f050bc56e55125dd18
Liam Hales <liam.hales96@gmail.com>
2026-09-17 20:25:12 +0100

    Added main `App` file and `AppView` view

---
+++ Long/App.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import SwiftData
+
+/// The main entry point to the
+/// entire application
+@main
+struct App: SwiftUI.App {
+
+  var body: some Scene {
+    WindowGroup {
+      AppView();
+    }
+  }
+}
---
+++ Long/Views/AppView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+import LucideSwift
+
+/// The main entry point view
+/// rendered by the `App`
+struct AppView: View {
+    
+  var body: some View {}
+}
+
