e2bfe15df2a1fd9000230a1ae968f146e32ac1f9
Liam Hales <liam.hales96@gmail.com>
2026-09-17 22:29:17 +0100

    Updated `AppView` to render the "Tasks" and "Archived" tabs

--- Long/Views/AppView.swift
+++ Long/Views/AppView.swift
@@ -4,7 +4,35 @@ import LucideSwift
 /// The main entry point view
 /// rendered by the `App`
 struct AppView: View {
-    
-  var body: some View {}
+
+  var body: some View {
+    TabView {
+      Tab(
+        content: {
+          TasksView();
+        },
+        label: {
+          Image(
+            lucide: .listCheck,
+            size: .init(width: 22, height: 22)
+          );
+          Text("Tasks");
+        },
+      );
+
+      Tab(
+        content: {
+          ArchivedView();
+        },
+        label: {
+          Image(
+            lucide: .archive,
+            size: .init(width: 22, height: 22)
+          );
+          Text("Archived");
+        },
+      );
+    }
+  }
 }
 
