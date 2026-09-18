fdc03d7cf633674be6db53bd13d240d557812c68
Liam Hales <liam.hales96@gmail.com>
2026-09-18 14:08:19 +0100

    Added `status` prop and computed `isCreatingTask` prop to `AppState`

--- Long/AppState.swift
+++ Long/AppState.swift
@@ -5,8 +5,21 @@ import Foundation
 @Observable
 final class AppState {
   var taskFilter: TaskFilter;
+  var status: AppStatus;
+  
+  var isCreatingTask: Bool {
+    get {
+      return (self.status == .creatingTask)
+    }
+    set {
+      self.status = (newValue == true)
+        ? .creatingTask
+        : .idle
+    }
+  }
 
   init() {
     self.taskFilter = .date;
+    self.status = .idle;
   }
 }
