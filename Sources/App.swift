7e42f60cac52b49b89361c7ffe2e3f4a147d2082
Liam Hales <liam.hales96@gmail.com>
2026-09-21 21:38:22 +0100

    Updated `App` to initialise model container and pass it to app state

--- Sources/App.swift
+++ Sources/App.swift
@@ -5,21 +5,28 @@ import SwiftData
 /// entire application
 @main
 struct App: SwiftUI.App {
+  private let _container: ModelContainer;
 
   @State
   private var _appState: AppState;
 
-  /// Initialises the app with new
-  // non-persistent app state
+  /// Initialises the app with new app state
+  /// and custom appearance configuration
   init() {
-    self._appState = AppState();
+    do {
+      self._container = try ModelContainer(for: TaskItem.self);
+      self.__appState = State(initialValue: AppState(context: self._container.mainContext));
+    }
+    catch {
+      fatalError("Failed to create ModelContainer: \(error)")
+    }
   }
 
   var body: some Scene {
     WindowGroup {
       AppView();
     }
-    .environment(self._appState)
-    .modelContainer(for: TaskItem.self);
+    .modelContainer(self._container)
+    .environment(self._appState);
   }
 }
