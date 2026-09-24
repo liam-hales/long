ac0d096a96bcd11ed616ffaa3dc478fbc2ea8bbb
Liam Hales <liam.hales96@gmail.com>
2026-09-24 17:25:42 +0100

    Removed `status` state and added new `navSelection` and `selectedTaskSheet` state

--- Sources/AppState.swift
+++ Sources/AppState.swift
@@ -6,45 +6,74 @@ import SwiftData
 @Observable
 final class AppState {
   private let _context: ModelContext;
-  
-  /// Describes the different statuses
-  /// the app can be in at any given time
-  enum Status: String {
-    case idle;
+
+  /// Describes what the user has
+  /// currently selected for navigation
+  enum NavSelection: Hashable {
+    case taskSheet(_ id: TaskSheetItem.ID);
+    case settings;
+    case archived;
   }
 
-  var status: Status;
+  var navSelection: NavSelection?;
   var taskFilter: TaskFilter;
-  
+
+  /// The currently selected task sheet
+  /// if the user has one selected
+  var selectedTaskSheet: TaskSheetItem? {
+    guard case .taskSheet(let id) = self.navSelection else {
+      return nil;
+    }
+
+    // Define the descriptor to fetch
+    // the task sheet item via its ID
+    let descriptor = FetchDescriptor<TaskSheetItem>(
+      predicate: #Predicate {
+        $0.persistentModelID == id
+      }
+    );
+
+    return try? self._context
+      .fetch(descriptor)
+      .first;
+  }
+
   /// Initialises the `AppState` with a given
   /// model context for the task item
   init(context: ModelContext) {
     self._context = context;
-    
+
     self.taskFilter = .all;
-    self.status = .idle;
+    self.navSelection = nil;
   }
-  
-  /// Used to create a task
-  /// with a give `title`
+
+  /// Used to create and save a
+  /// new task with a give `title`
   func createTask(title: String) -> Void {
     self._createTask(title: title);
   }
-  
-  /// Used to create and save a new task item
+
+  /// Used to create and save a new task
   /// with a given `title` and `dueDate`
   private func _createTask(title: String, dueDate: Date? = nil) -> Void {
     let trimmed = title.trimmingCharacters(in: .whitespacesAndNewlines);
-    
+
     // Check if the trimmed title
     // is empty and if so return
     if (trimmed.isEmpty == true) {
       return;
     }
 
-    // Create the new task
-    // with the trimmed title
+    // Make sure the user has
+    // a task sheet selected
+    guard let sheet = self.selectedTaskSheet else {
+      return;
+    }
+
+    // Create the new task for the selected task sheet
+    // with the trimmed title and due date
     let newTask = TaskItem(
+      sheet: sheet,
       title: trimmed,
       dueDate: dueDate
     );
